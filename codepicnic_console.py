2727b467014302230e21fd5f137c3b3a126bc3f8
Hector Paz <hector.paz@clicksandbricks.pe>
2015-12-21 03:10:55 -0500

    create consoles

--- codepicnic_console.py
+++ codepicnic_console.py
@@ -3,24 +3,25 @@
 import json
 import requests
 import requests.packages.urllib3
+import pprint
 
 requests.packages.urllib3.disable_warnings()
 
 def main():
     module = AnsibleModule(
         argument_spec = dict(
-            client_id = dict(required=True),
-            client_secret = dict(required=True),
-            api_url = dict (default='https://codepicnic.com/oauth/token')
+            access_token = dict(required=True),
+            size = dict(required=True),
+            type = dict(required=True),
+            name = dict(default=''),
+            custom_image = dict(default=''),
+            api_url = dict (default='https://codepicnic.com/api/consoles')
         )
     )
-    cp_payload = dict(
-        grant_type = 'client_credentials',
-        client_id = module.params['client_id'],
-        client_secret = module.params['client_secret'],
-    )
+    cp_headers = {'Authorization': 'Bearer ' + module.params['access_token'], 'content-type': 'application/json'}
+    cp_bite ={"bite":{"container_size":module.params['size'], "container_type":module.params['type'], "hostname":module.params['name']}}
     try: 
-        cp_token_response = requests.post(module.params['api_url'], data=cp_payload)
+        cp_token_response = requests.post(module.params['api_url'], data=json.dumps(cp_bite), headers=cp_headers)
     except requests.exceptions.ConnectionError as e:
         module.fail_json(msg="Name or service not known:" + module.params['api_url'])
 
@@ -32,7 +33,7 @@ def main():
         module.fail_json(msg=cp_token_dict['error_description'])
 
     cp_token_dict['changed'] = True 
-    cp_token_dict['ansible_facts'] = dict(codepicnic_access_token=cp_token_dict['access_token'])
+    #cp_token_dict['ansible_facts'] = dict(codepicnic_access_token=cp_token_dict['access_token'])
     module.exit_json(**cp_token_dict)
 
 from ansible.module_utils.basic import *
